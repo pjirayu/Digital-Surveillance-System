64a5b4907a3ae61b2c6714a6ff2a5f474364dd1f
Jirayu Petchhan <75000395+pjirayu@users.noreply.github.com>
2021-03-05 22:23:14 +0800

    Add files via upload

---
+++ 5-2/k-means.py
@@ -0,0 +1,83 @@
+import numpy as np
+import matplotlib.pyplot as plt
+
+
+seed_num = 3
+dot_num = 20
+
+x = np.random.randint(0, 1000, dot_num)
+y = np.random.randint(0, 1000, dot_num)
+
+kx = np.random.randint(0, 1000, seed_num)
+ky = np.random.randint(0, 1000, seed_num)
+
+
+
+def dis(x, y, kx, ky):
+    return int(((kx-x)**2 + (ky-y)**2)**0.5)
+
+
+def cluster(x, k, kx, ky):
+    team = []
+    for i in range(3):
+        team.append([])
+    mid_dis = 99999999
+    for i in range(dot_num):
+        for j in range(seed_num):
+            distant = dis(x[i], y[i], kx[j], ky[j])
+            if distant < mid_dis:
+                mid_dis = distant
+                flag = j
+        team[flag].append([x[i], y[i]])
+        mid_dis = 99999999
+    return team
+
+def re_seed(team, kx, ky):
+    sumx = 0
+    sumy = 0
+    new_seed = []
+    for index, nodes in enumerate(team):
+        if nodes == []:
+            new_seed.append([kx[index], ky[index]])
+        for node in nodes:
+            sumx += node[0]
+            sumy += node[1]
+        new_seed.append([int(sumx/len(nodes)), int(sumy/len(nodes))])
+        sumx = 0
+        sumy = 0
+    nkx = []
+    nky = []
+    for i in new_seed:
+        nkx.append(i[0])
+        nky.append(i[1])
+    return nkx, nky
+
+def kmeans(x, y, kx, ky, fig):
+    team = cluster(x, y, kx, ky)
+    nkx, nky = re_seed(team, kx, ky)
+
+    cx = []
+    cy = []
+    line = plt.gca()
+    for index, nodes in enumerate(team):
+        for node in nodes:
+            cx.append([node[0], nkx[index]])
+            cy.append([node[1], nky[index]])
+        for i in range(len(cx)):
+            line.plot(cx[i], cy[i], color='r', alpha=0.3)
+        cx = []
+        cy = []
+    
+    feature = plt.scatter(x, y)
+    k_feature = plt.scatter(kx, ky)
+    nk_feaure = plt.scatter(np.array(nkx), np.array(nky), s=50)
+    plt.show()
+
+    if nkx == list(kx) and nky == (ky):
+        return
+    else:
+        fig += 1
+        kmeans(x, y, nkx, nky, fig)
+
+
+kmeans(x, y, kx, ky, fig=0)
